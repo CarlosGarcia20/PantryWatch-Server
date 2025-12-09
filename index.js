eab24919153249fdba5ac7b67ec1c264bb6a2b30
CarlosGarcia20 <cargarcia085@gmail.com>
2025-12-09 10:00:57 -0700

    Agregado el endpoint de productos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { sensorRouter } from './src/routes/sensor.routes.js';
 import { prologService } from './src/services/prologService.js';
+import { productoRouter } from './src/routes/productos.routes.js';
 
 // Inicializar Express
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 
 // Rutas
 app.use('/api', sensorRouter)
+app.use('/api/productos', productoRouter);
 
 // Arrancar servidor
 app.listen(PORT, async() => {
