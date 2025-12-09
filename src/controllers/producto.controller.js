32daf2ecef5f3198c0bb74a04aaa53a5c2f88cf0
CarlosGarcia20 <cargarcia085@gmail.com>
2025-12-09 12:59:52 -0700

    En proceso el endpoint para crear productos

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -1,14 +1,16 @@
-import { productoModelo } from "../models/producto.model";
+import { productoModelo } from "../models/producto.model.js";
 
 export class productoController {
     static async crear(req, res) {
         try {
-            const resultado = await productoModelo.crear();
+            const resultado = await productoModelo.crear(req.body);
 
             if(!resultado.success) {
                 
             }
         } catch (error) {
+            console.log(error);
+            
             return res.status(500).json({ mensaje: "Error Interno del Servidr" });
         }
     }
--- src/models/producto.model.js
+++ src/models/producto.model.js
@@ -3,7 +3,11 @@ import pool from "../config/db.js";
 export class productoModelo {
     static async crear() {
         try {
+            const { rows } = await pool.query(
+                `INSERT INTO `
+            );
             
+            return { success: true, data: rows }
         } catch (error) {
             return { success: false, error }
         }
