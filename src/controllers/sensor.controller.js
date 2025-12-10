3f5a191ae1bae16f23820d00e56f667c163d4f89
CarlosGarcia20 <cargarcia085@gmail.com>
2025-12-09 23:14:49 -0700

    Actualizacion al endpoint de sensores para que tome el sensor de peso

--- src/controllers/sensor.controller.js
+++ src/controllers/sensor.controller.js
@@ -1,14 +1,28 @@
+import { prologService } from "../services/prologService.js";
+
 export class sensorController {
     static async recibirDatosSensor(req,res) {
         try {
-            const { zona, temperatura, humedad } = req.body;
+            const { zona, temperatura, humedad, peso_actual } = req.body;
 
-            // Validación básica
             if (!zona || temperatura === undefined || humedad === undefined) {
                 return res.status(400).json({ error: "Faltan datos (zona, temperatura, humedad)" });
             }
 
-            console.log(`📥 API: Recibido de ${zona} -> T:${temperatura} H:${humedad}`);
+            console.log(`📥 API: Recibido de ${zona} -> T:${temperatura} H:${humedad} P:${peso_actual}`);
+
+            // if (req.io) {
+            //     req.io.emit('actualizacion_sensores', {
+            //         zona: zona,
+            //         temperatura: temperatura,
+            //         humedad: humedad,
+            //         peso: peso_actual,
+            //         timestamp: new Date()
+            //     });
+            //     console.log("📡 Datos enviados a la App Móvil vía Socket");
+            // } else {
+            //     console.warn("⚠️ No se encontró instancia de Socket.io (req.io)");
+            // }
 
             // Llamamos al servicio (Caja negra)
             const alertas = await prologService.procesarDatosSensor(zona, temperatura, humedad);
