040c5b7c440c36b024b735b251d48244d9bfc117
CarlosGarcia20 <cargarcia085@gmail.com>
2025-12-09 23:13:23 -0700

    Creacion del socket

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import { sensorRouter } from './src/routes/sensor.routes.js';
+import http from 'http'
+import { Server } from 'socket.io';
+ 
+// Archivos de servicios
 import { prologService } from './src/services/prologService.js';
+// Arvhivos de rutas
+import { sensorRouter } from './src/routes/sensor.routes.js';
 import { productoRouter } from './src/routes/productos.routes.js';
 import { contenedorRouter } from './src/routes/contenedor.routes.js';
 
 // Inicializar Express
 const app = express();
 const PORT = 3000;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
 
 app.use(cors());
 app.use(express.json());
@@ -17,8 +35,19 @@ app.use('/api', sensorRouter)
 app.use('/api/productos', productoRouter);
 app.use('/api/contenedor', contenedorRouter)
 
+
+// Evento de conexión
+io.on('connection', (socket) => {
+    console.log('📱 App Móvil conectada al Socket: ' + socket.id);
+    
+    socket.on('disconnect', () => {
+        console.log('📱 App desconectada');
+    });
+});
+
+
 // Arrancar servidor
-app.listen(PORT, async() => {
+server.listen(PORT, '0.0.0.0', async() => {
     console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 
     await prologService.init();
